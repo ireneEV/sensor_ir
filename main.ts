9dce5174e7601b6c322342476c2224fb1c632175
ireneEV <94906639+ireneEV@users.noreply.github.com>
2022-05-10 13:36:59 +0200

    ahora va hacia atrás cuando esta muy cercaa

--- main.ts
+++ main.ts
@@ -1,26 +1,18 @@
 let medida = 0
-maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 255)
+maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 150)
 basic.forever(function () {
     medida = maqueen.Ultrasonic(PingUnit.Centimeters)
-    basic.showNumber(medida)
     if (medida < 5) {
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CCW, 53)
         music.playTone(262, music.beat(BeatFraction.Sixteenth))
         basic.pause(200)
         music.playTone(262, music.beat(BeatFraction.Sixteenth))
         maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 50)
-    } else {
-        music.playTone(262, music.beat(BeatFraction.Sixteenth))
-        maqueen.motorStop(maqueen.Motors.All)
     }
     music.playTone(262, music.beat(BeatFraction.Sixteenth))
     if (medida < 10) {
-    	
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 53)
+    } else {
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 150)
     }
-    if (true) {
-    	
-    }
-    basic.pause(2000)
-})
-control.inBackground(function () {
-    basic.showNumber(medida)
 })
